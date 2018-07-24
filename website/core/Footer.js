a1a5cbfa0671831f3f0962b307acfed5d58bcd18
David Azancot <david.azancot@4d.com>
2018-07-24 15:10:57 +0200

    Fix footer links

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -79,11 +79,12 @@ class Footer extends React.Component {
             <a href="http://www.4d.com/legal.html">Legal</a>
 
             <a href="https://github.com/4D/4d-for-ios">GitHub</a>
-    <a
+            <a
               className="github-button"
-              href={this.props.config.repoUrl}
+              href="https://github.com/4D/4d-for-ios"
               data-icon="octicon-star"
               data-count-href="/4D/4d-for-ios/stargazers"
+              data-count-api="/repos/4D/4d-for-ios#stargazers_count"
               data-show-count={true}
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub">
