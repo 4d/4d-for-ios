93a9a0a5cd8f03c7f8f1354e21ac15d00a318461
David Azancot <david.azancot@4d.com>
2019-04-16 17:09:57 +0200

    disable french translation

--- website/languages.js
+++ website/languages.js
@@ -12,7 +12,7 @@ const languages = [
     tag: 'en',
   },
   {
-    enabled: true,
+    enabled: false,
     name: '日本語',
     tag: 'ja',
   },
@@ -42,7 +42,7 @@ const languages = [
     tag: 'da',
   },
   {
-    enabled: true,
+    enabled: false,
     name: 'Deutsch',
     tag: 'de',
   },
@@ -52,7 +52,7 @@ const languages = [
     tag: 'el',
   },
   {
-    enabled: true,
+    enabled: false,
     name: 'Español',
     tag: 'es-ES',
   },
@@ -67,7 +67,7 @@ const languages = [
     tag: 'fi',
   },
   {
-    enabled: true,
+    enabled: false,
     name: 'Français',
     tag: 'fr',
   },
@@ -122,7 +122,7 @@ const languages = [
     tag: 'pl',
   },
   {
-    enabled: true,
+    enabled: false,
     name: 'Português',
     tag: 'pt-PT',
   },
